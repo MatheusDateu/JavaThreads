f5fc34fd3dff996eab92a5ad401947976ea52638
MatheusDateu <mdelmondes5@gmail.com>
2022-11-08 00:28:39 -0400

    Threads em Java.

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Thread t1 = null;
+        try {
+            t1 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 100000; i++) {
+                        System.out.println("Index1: " + i);
+                    }
+                }
+            });
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int i = 0;
+                    while (i < 100000) {
+                        System.out.println(i + " esse e o Index2");
+                        i++;
+                    }
+
+                }
+            }).start();
+            t1.start();
+
+            Runnable t3 = () -> {
+                for (int i = 0; i < 2; i++) {
+                    System.out.println("Opa");
+                }
+            };
+            System.out.println("Hello world!");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if(t1.isAlive()){
+                t1.interrupt();
+            }
+        }
+    }
+}
